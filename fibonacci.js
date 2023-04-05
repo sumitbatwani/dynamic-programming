9a3f80535a8a385df30d4fb4d336830258c3cc93
Sumit Kumar <sumitbatwani@gmail.com>
2023-04-05 22:57:10 +0530

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -3,10 +3,10 @@ f[0] = 0
 f[1] = 1
 
 function fib(n) {
-	for(let i=2;i<n;i++) {
-  	f[i] = f[i-1] + f[i-2]
-  }
-  return f
+ for(let i=2;i<n;i++) {
+   f[i] = f[i-1] + f[i-2]
+ }
+return f
 }
 
 const r = fib(5)
